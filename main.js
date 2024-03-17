cf0a428aa76d30baa72eab1ccdd7a07005ec910e
Alex <lesha.rudiko@gmail.com>
2024-03-17 19:36:55 +0300

    update

--- main.js
+++ main.js
@@ -9,29 +9,23 @@ const DomElement = function(selector, height, width, bg, fontSize){
 
 DomElement.prototype.createElement = function (){
     const sliceSelector = this.selector.slice(0, 1)
+
+    let newElem = ''
+
     if(sliceSelector === '.'){
-        const newDiv = document.createElement('div')
-        newDiv.classList.add(this.selector.slice(1))
-        newDiv.textContent = 'hello world'
-        document.body.append(newDiv)
-        newDiv.style.cssText = `
-        height: ${this.height};
-        width: ${this.width};
-        background: ${this.bg};
-        font-size: ${this.fontSize};
-        `
-    } else if(sliceSelector === '#'){
-        const newDiv = document.createElement('div')
-        newDiv.setAttribute('id', this.selector.slice(1))
-        newDiv.textContent = 'hello world'
-        document.body.append(newDiv)
-        newDiv.style.cssText = `
-        height: ${this.height};
-        width: ${this.width};
-        background: ${this.bg};
-        font-size: ${this.fontSize};
-        `
+        newElem = document.createElement('div')
+        newElem.classList.add(this.selector.slice(1))
+    }else if(sliceSelector === '#'){
+        newElem = document.createElement('p')
+        newElem.setAttribute('id', this.selector.slice(1))
     }
+
+    newElem.style.cssText = `
+    height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};
+    `
 }
 
 const newElem = new DomElement('#selector', "100px", "300px", "#0dd144", "25px")
